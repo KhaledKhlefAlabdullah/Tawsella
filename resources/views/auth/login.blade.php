25a4683652370bed6d32f3368c0aab44915195c9
Abdulrahmanl <ramasy111020033@gmail.com>
2024-03-30 18:33:45 +0300

    g

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
 
                             <div class="d-flex justify-content-center py-4">
                                 <a href="/" class="logo d-flex align-items-center w-auto">
-                                    <img src="img/logo.png" alt="">
+                                    <img src="img/logoo.png" alt="">
                                 </a>
                             </div><!-- End Logo -->
 
