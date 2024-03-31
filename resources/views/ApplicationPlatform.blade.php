d77f3ab3dc24c7a77f3a8b8aab84f0eb83bc750c
Abdulrahmanl <ramasy111020033@gmail.com>
2024-03-31 14:12:02 +0300

    dach

--- resources/views/ApplicationPlatform.blade.php
+++ resources/views/ApplicationPlatform.blade.php
@@ -45,7 +45,6 @@
                     </div>
                 </div>
 
-
                 <!-- Modal -->
                 <div class="modal fade" id="downloadModal" tabindex="-1" aria-labelledby="downloadModalLabel"
                     aria-hidden="true">
