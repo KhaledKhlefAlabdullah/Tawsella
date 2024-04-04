0b22bdb88ea7f3ea0d1bacfec278e482113f5e82
Abdulrahmanl <ramasy111020033@gmail.com>
2024-04-05 01:43:01 +0300

    aa

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -80,7 +80,7 @@
                 <div class="row">
                     <div class="d-flex align-items-center justify-content-between">
                         <i class="bi bi-list toggle-sidebar-btn"></i>
-                        <a href="{{ url('/') }}" class="logo d-flex align-items-center">
+                        <a class="logo d-flex align-items-center">
                             <img src="{{ asset('/img/logoo.png') }}" style="width: 150px;height: 50px;padding: 3px;"
                                 alt="">
                         </a>
