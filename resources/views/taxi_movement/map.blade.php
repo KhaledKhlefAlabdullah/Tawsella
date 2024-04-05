4caef343fec746ab65d21e33085e12eac096c925
khaled <khaledabdullah2001104@gmail.com>
2024-04-05 20:02:43 +0300

    .

--- resources/views/taxi_movement/map.blade.php
+++ resources/views/taxi_movement/map.blade.php
@@ -19,7 +19,7 @@
         <div id="map" class="map"></div>
         @vite('resources/js/app.js')
         <script>
-            var map = L.map('map').setView([{{ $data->lat }}, {{ $data->long }}], 5);
+            var map = L.map('map').setView([{{ $data->lat }}, {{ $data->long }}], 10);
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo(map);
@@ -34,7 +34,7 @@
 
             Echo.private(`TaxiLocation.${admin_id}`)
                 .listen('.App\\Events\\GetTaxiLocationsEvent', (e) => {
-                    // if (e.driver_id == driver_id) {
+                    if (e.driver_id == driver_id) {
                         console.log('com');
 
                         // Remove previous marker
@@ -46,7 +46,7 @@
 
                         // Update prevMarker reference
                         prevMarker = newMarker;
-                    // }
+                    }
                 });
         </script>
 
