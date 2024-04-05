690ab9a3616995806bdc8fa39a991cb89f9fc61f
khaled <khaledabdullah2001104@gmail.com>
2024-04-05 19:52:44 +0300

    .

--- routes/channels.php
+++ routes/channels.php
@@ -14,28 +14,28 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return  $user->id ===  $id;
 });
 
 
 Broadcast::channel('Taxi-movement.{admin_id}', function ($user, $admin_id) {
-    return (int) $user->id === (int) $admin_id;
+    return  $user->id ===  $admin_id;
 });
 
 
 Broadcast::channel('TaxiLocation.{admin_id}', function ($user, $admin_id) {
 
-    return (int) $user->id === (int) $admin_id;
+    return $user->id === $admin_id;
 });
 
 Broadcast::channel('customer.{customer_id}', function ($user, $customer_id) {
-    return (int) $user->id === (int) $customer_id;
+    return  $user->id ===  $customer_id;
 });
 
 Broadcast::channel('driver.{driver_id}', function ($user, $driver_id) {
-    return (int) $user->id === (int) $driver_id;
+    return  $user->id ===  $driver_id;
 });
 
 Broadcast::channel('movemnt.{admin_id}', function ($user, $admin_id) {
-    return (int) $user->id === (int) $admin_id;
+    return  $user->id ===  $admin_id;
 });
