c42ac938535f97450c8d3a6e02a448674d377b6f
Abdulrahmanl <ramasy111020033@gmail.com>
2024-04-09 23:20:28 +0300

    scsc

--- resources/views/ApplicationPlatform.blade.php
+++ resources/views/ApplicationPlatform.blade.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Smart Code</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
 <main class="main" style="margin: 15px;">
     <div class="container">
         <div class="row justify-content-center mt-5">
@@ -84,9 +96,14 @@
     </div>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     function showModal(downloadLink) {
         $('#downloadModal').modal('show');
         setTimeout(() => $('#downloadModal').modal('hide'), 15000);
     }
 </script>
+</body>
+
+</html>
