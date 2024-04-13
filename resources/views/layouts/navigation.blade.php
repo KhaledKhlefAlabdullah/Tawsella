fcdc7e646b58044093ff37084ecc509adfce8db1
Abdulrahmanl <ramasy111020033@gmail.com>
2024-04-13 21:42:29 +0300

    list-style: none;

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -113,7 +113,6 @@
                     class="bi bi-menu-button-wide " style="margin-left: 10px"></i>
             </a>
             <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-                <li>jfnd;jkndfnb'knbg</li>
                 <li>
                     <a href="{{ route('drivers.index') }}">
                         <div class="ms-auto" style="margin-right: 30px">
