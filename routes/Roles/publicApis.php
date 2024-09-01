d274393565b9c7a7862cf9d4e4cb052a6426bc5c
Khaled <khaledabdullah2001104@gamil.com>
2024-09-01 11:51:36 +0300

    Make about us public api

--- routes/Roles/publicApis.php
+++ routes/Roles/publicApis.php
@@ -2,5 +2,9 @@
 
 use App\Http\Controllers\ContactUsMessageController;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\OfferController;
 
 Route::post('contact-us/send', [ContactUsMessageController::class, 'store']);
+Route::get('/about-us', [AboutUsController::class, 'index']);
+Route::get('/offers', [OfferController::class, 'index']);
