0b7e37809722e29f8681f1fecd87204db5d8ccbd
Khaled <khaledabdullah2001104@gamil.com>
2024-09-04 01:55:55 +0300

    Make scheduling task to deactivate expired drivers

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('send:activation-reminders')->daily();
+Schedule::command('app:deactivate-expired-drivers')->daily();
