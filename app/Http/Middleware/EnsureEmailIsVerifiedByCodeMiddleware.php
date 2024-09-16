c303da50ee31db8ca5b265ca0b9bb3bb4e5b63fd
Khaled <khaledabdullah2001104@gamil.com>
2024-09-17 00:02:42 +0300

    Fix send verification email

--- app/Http/Middleware/EnsureEmailIsVerifiedByCodeMiddleware.php
+++ app/Http/Middleware/EnsureEmailIsVerifiedByCodeMiddleware.php
@@ -4,13 +4,13 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use App\Interfaces\MustVerifyEmailByCode;
+use App\Interfaces\IMustVerifyEmailByCode;
 
 class EnsureEmailIsVerifiedByCodeMiddleware
 {
     public function handle(Request $request, Closure $next, $redirectToRoute = null)
     {
-        if (!$request->user() || ($request->user() instanceof MustVerifyEmailByCode && $request->user()->hasVerifiedEmailByCode())) {
+        if (!$request->user() || ($request->user() instanceof IMustVerifyEmailByCode && $request->user()->hasVerifiedEmailByCode())) {
             return api_response(message: 'لم يتم تأكيد بريدك قم بتأكيده أولاً قبل محاولة الوصول');
         }
         return $next($request);
