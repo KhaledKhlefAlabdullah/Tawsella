9ab43894cdbc4aceee1a98e6c9ddbb6c8f2beaa5
Khaled <khaledabdullah2001104@gamil.com>
2025-02-03 17:58:29 +0300

    Make seeder for advertisements

---
+++ database/seeders/AdvertisementsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class AdvertisementsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('advertisements')->insert([
+                'id' => Str::uuid(),
+                'admin_id' => '1', // تأكد من وجود مستخدم بهذا المعرف في جدول users
+                'title' => 'إعلان ' . ($i + 1),
+                'image' => '/images/services/images/service.jpg',
+                'logo' => '/images/services/logos/logo.jpg',
+                'description' => 'هذا وصف للإعلان رقم ' . ($i + 1),
+                'validity_date' => Carbon::now()->addDays(rand(10, 30)),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
