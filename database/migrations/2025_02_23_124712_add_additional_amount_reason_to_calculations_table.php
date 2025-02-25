33d090126a70898539f7451b71604e4141fbddc6
Khaled <khaledabdullah2001104@gamil.com>
2025-02-25 12:38:21 +0300

    Make the calculation is the additional_amount

--- database/migrations/2025_02_23_124712_add_additional_amount_reason_to_calculations_table.php
+++ database/migrations/2025_02_23_124712_add_additional_amount_reason_to_calculations_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('calculations', function (Blueprint $table) {
-            $table->float('additional_amount')->nullable()->after('distance');
+            $table->string('additional_amount')->nullable()->after('distance');
             $table->text('reason')->nullable()->after('additional_amount');
         });
     }
